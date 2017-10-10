d18bb0178f5b1f11ef791ce3de8864fed43075ef
GoodLuckDay <haon4658@gmail.com>
2017-10-10 23:06:40 +0900

    first mobile commit

---
+++ Practice1/HelloWorld/app/src/main/java/com/example/haon4658/helloworld/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.haon4658.helloworld;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClicked(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("010-1234-5678"));
+        startActivity(intent);
+    }
+}
